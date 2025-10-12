d0bc077a5fbc0d003c106eaf3e74e128d02d7921
Hamderber <50248474+Hamderber@users.noreply.github.com>
2025-10-12 11:13:57 -0500

    image

---
+++ src/rendering/image.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <stdint.h>
+#include <vulkan/vulkan.h>
+#include "core/types/state_t.h"
+
+void imageLayoutTransition(State_t *state, VkImage image, VkFormat format, VkImageLayout oldLayout, VkImageLayout newLayout);
+
+void imageCreate(State_t *state, uint32_t width, uint32_t height, VkFormat format, VkImageTiling tiling, VkImageUsageFlags usage,
+                 VkMemoryPropertyFlags properties, VkImage *image, VkDeviceMemory *imageMemory);
