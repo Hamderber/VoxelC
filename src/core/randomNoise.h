0c2cd0d0eae7ecd81aee7c84ec3eddce80813eeb
Hamderber <50248474+Hamderber@users.noreply.github.com>
2025-11-05 11:33:19 -0600

    Update randomNoise.h

--- src/core/randomNoise.h
+++ src/core/randomNoise.h
@@ -1,10 +1,9 @@
 #pragma once
 
 #include <stdint.h>
-#include "cmath/cmath.h"
 
-void randomNoise_init(uint32_t worldSeed);
+void randomNoise_init(const uint32_t WORLD_SEED);
 
-float randomNoise_stone_samplePackedPos(const Chunk_t *pC, uint16_t blockPosPacked12);
+float randomNoise_stone_samplePackedPos(const Chunk_t *pC, const uint16_t BLOCK_POS_PACKED12);
 
-float randomNoise_carving_sampleXYZ(const Chunk_t *pC, uint16_t blockPosPacked12);
+float randomNoise_carving_sampleXYZ(const Chunk_t *pC, const uint16_t BLOCK_POS_PACKED12);
